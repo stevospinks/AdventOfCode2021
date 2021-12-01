35c6c14ef1cc78846b0a08a59302b2d687bc95ba
stevospinks <github@stephenspinks.com>
2021-12-01 14:05:45 +0000

    Day 1 Part 1

---
+++ Solutions/Day01.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using Utilities;
+
+namespace Solutions
+{
+    public class Day01
+    {
+        public static void Main(string[] args)
+        {
+            var input = FileReader.ReadInputAsInt(1).ToList();
+
+            var result = 0;
+            // Ignore the first input, nothing to compare it to
+            for (int i = 1; i < input.Count(); i++)
+            {
+                if (input[i] > input[i-1])
+                {
+                    result++;
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Utilities/FileReader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Utilities
+{
+    public static class FileReader
+    {
+        public static IEnumerable<int> ReadInputAsInt(int day)
+        {
+            var path = GetInputPath(day);
+            var lines = File.ReadAllLines(path);
+
+            var result = lines.Select(l => Convert.ToInt32(l));
+
+            return result;
+        }
+
+        private static string GetInputPath(int day)
+        {
+            var root = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, @"..\..\..\..\"));
+            return Path.Combine(root, $@"Input\Day{day.ToString("D2")}.txt");
+        }
+    }
+}

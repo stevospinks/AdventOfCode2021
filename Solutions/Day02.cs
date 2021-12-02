42b003e18833d69f3bf1d8ab187fa5f6078d508c
stevospinks <github@stephenspinks.com>
2021-12-02 10:01:01 +0000

    Day 2 Part 2

--- Solutions/Day02.cs
+++ Solutions/Day02.cs
@@ -13,12 +13,15 @@ namespace Solutions
 
             var partOne = PartOne(input);
             Console.WriteLine($"Day 02, Part One: {partOne}");
+
+            var partTwo = PartTwo(input);
+            Console.WriteLine($"Day 02, Part Two: {partTwo}");
         }
 
         private static int PartOne(List<string> input)
         {
-            var forward = 0;
-            var down = 0;
+            var horizontalPosition = 0;
+            var depth = 0;
 
             foreach (var instruction in input)
             {
@@ -30,20 +33,53 @@ namespace Solutions
                 switch (command)
                 {
                     case "forward":
-                        forward += value;
+                        horizontalPosition += value;
                         break;
                     case "up":
-                        down -= value;
+                        depth -= value;
                         break;
                     case "down":
-                        down += value;
+                        depth += value;
                         break;
                     default:
                         break;
                 }
             }
 
-            return forward * down;
+            return horizontalPosition * depth;
+        }
+
+        private static int PartTwo(List<string> input)
+        {
+            var horizontalPosition = 0;
+            var depth = 0;
+            var aim = 0;
+
+            foreach (var instruction in input)
+            {
+                var instructions = instruction.Split(' ');
+
+                var command = instructions[0];
+                var value = Convert.ToInt32(instructions[1]);
+
+                switch (command)
+                {
+                    case "forward":
+                        horizontalPosition += value;
+                        depth += value * aim;
+                        break;
+                    case "up":
+                        aim -= value;
+                        break;
+                    case "down":
+                        aim += value;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return horizontalPosition * depth;
         }
     }
 }

0c63f75d134c5e74363ab85fd5115f2464342fe7
stevospinks <github@stephenspinks.com>
2021-12-06 14:04:25 +0000

    Day 5 Part 2

--- Solutions/Day05.cs
+++ Solutions/Day05.cs
@@ -35,44 +35,80 @@ namespace Solutions
                 var x2 = int.Parse(coordinates2[0]);
                 var y2 = int.Parse(coordinates2[1]);
 
-                GenerateStraightLineCoverage(coverageCoordinates, x1, x2, y1, y2);
+                GenerateLineCoverage(coverageCoordinates, x1, x2, y1, y2, true);
             }
 
             var result = coverageCoordinates.Count(c => c.Value > 1);
             return result;
         }
 
-        private static void GenerateStraightLineCoverage(Dictionary<Tuple<int, int>, int> coverageCoordinates, int x1, int x2, int y1, int y2)
+        private static void GenerateLineCoverage(Dictionary<Tuple<int, int>, int> coverageCoordinates, int x1, int x2, int y1, int y2, bool ignoreDiagonals)
         {
-            if (x1 != x2 && y1 != y2)
+            if (x1 == x2 || y1 == y2)
             {
-                return;
-            }
+                // Process this horizontal or vertical line
+                var minX = x1 < x2 ? x1 : x2;
+                var maxX = x1 < x2 ? x2 : x1;
+                var minY = y1 < y2 ? y1 : y2;
+                var maxY = y1 < y2 ? y2 : y1;
+                var xEqual = minX == maxX;
 
-            var minX = x1 < x2 ? x1 : x2;
-            var maxX = x1 < x2 ? x2 : x1;
-            var minY = y1 < y2 ? y1 : y2;
-            var maxY = y1 < y2 ? y2 : y1;
-
-            var xEqual = minX == maxX;
-
-            for (int i = (xEqual ? minY : minX); i <= (xEqual ? maxY : maxX); i++)
-            {
-                var newCoordinate = xEqual ? new Tuple<int, int>(x1, i) : new Tuple<int, int>(i, y1);
-                if (coverageCoordinates.ContainsKey(newCoordinate))
+                for (int i = (xEqual ? minY : minX); i <= (xEqual ? maxY : maxX); i++)
                 {
-                    coverageCoordinates[newCoordinate]++;
+                    var newCoordinate = xEqual ? new Tuple<int, int>(x1, i) : new Tuple<int, int>(i, y1);
+                    if (coverageCoordinates.ContainsKey(newCoordinate))
+                    {
+                        coverageCoordinates[newCoordinate]++;
+                    }
+                    else
+                    {
+                        coverageCoordinates.Add(newCoordinate, 1);
+                    }
                 }
-                else
+            }
+            else if (!ignoreDiagonals)
+            {
+                // Process this diagonal line
+                var xIncreasing = x1 < x2;
+                var yIncreasing = y1 < y2;
+                while (xIncreasing ? x1 <= x2 : x1 >= x2)
                 {
-                    coverageCoordinates.Add(newCoordinate, 1);
+                    var newCoordinate = new Tuple<int, int>(x1, y1);
+                    if (coverageCoordinates.ContainsKey(newCoordinate))
+                    {
+                        coverageCoordinates[newCoordinate]++;
+                    }
+                    else
+                    {
+                        coverageCoordinates.Add(newCoordinate, 1);
+                    }
+
+                    x1 = xIncreasing ? x1 + 1 : x1 - 1;
+                    y1 = yIncreasing ? y1 + 1 : y1 - 1;
                 }
             }
         }
 
         private static int PartTwo(List<string> input)
         {
-            return -1;
+            var coverageCoordinates = new Dictionary<Tuple<int, int>, int>();
+            foreach (var line in input)
+            {
+                var coordinateSet = line.Split(" -> ");
+
+                var coordinates1 = coordinateSet[0].Split(',');
+                var x1 = int.Parse(coordinates1[0]);
+                var y1 = int.Parse(coordinates1[1]);
+
+                var coordinates2 = coordinateSet[1].Split(',');
+                var x2 = int.Parse(coordinates2[0]);
+                var y2 = int.Parse(coordinates2[1]);
+
+                GenerateLineCoverage(coverageCoordinates, x1, x2, y1, y2, false);
+            }
+
+            var result = coverageCoordinates.Count(c => c.Value > 1);
+            return result;
         }
     }
 }

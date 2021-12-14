6047a035fecffb6d598ac4a32deeb7115f358bf5
stevospinks <github@stephenspinks.com>
2021-12-14 17:06:09 +0000

    Day 14 Part 2

--- Solutions/Day14.cs
+++ Solutions/Day14.cs
@@ -23,67 +23,106 @@ namespace Solutions
         private static int PartOne(List<string> input)
         {
             var rules = ProcessInput(input, out var template);
-            var polymer = GeneratePolymer(rules, template);
-            var result = CalculateResult(rules, polymer);
+            var polymer = GeneratePolymer(rules, template, 10);
+            var result = CalculateResult(polymer);
             return result;
         }
 
-        private static int PartTwo(List<string> input)
+        private static long PartTwo(List<string> input)
         {
-            return -1;
+            var rules = ProcessInput(input, out var template);
+
+            var expansions = 20; // = 40 / 2 (meaning there will be only two polymer steps required)
+            var expandedRules = ExpandRules(rules, expansions);
+
+            // Expand the polymer using one of the remaining two steps
+            var polymer = GeneratePolymer(expandedRules, template, 1);
+
+            var result = CalculateResultWithOneRemainingStep(rules, expandedRules, polymer);
+            return result;
         }
 
-        private static Dictionary<string, char> ProcessInput(List<string> input, out IList<char> template)
+        private static Dictionary<string, char[]> ProcessInput(List<string> input, out char[] template)
         {
             template = input[0].ToCharArray();
 
-            var rules = new Dictionary<string, char>();
+            var rules = new Dictionary<string, char[]>();
             foreach (var line in input.Skip(1).Except(new List<string> { string.Empty }))
             {
                 var rule = line.Split(" -> ");
-                rules.Add(rule[0], rule[1].Single());
+                rules.Add(rule[0], rule[1].ToCharArray());
             }
 
             return rules;
         }
 
-        private static IList<char> GeneratePolymer(Dictionary<string, char> rules, IList<char> template)
+        private static Dictionary<string, char[]> ExpandRules(Dictionary<string, char[]> rules, int expansionLevels)
         {
-            IList<char> polymer = null;
-            for (int step = 0; step < 10; step++)
+            var expandedRules = new Dictionary<string, char[]>();
+            foreach (var rule in rules)
+            {
+                var expandedValue = GeneratePolymer(rules, rule.Key.ToCharArray(), expansionLevels);
+                var valueWithoutKey = expandedValue.Skip(1).SkipLast(1).ToArray();
+                expandedRules.Add(rule.Key, valueWithoutKey);
+            }
+
+            return expandedRules;
+        }
+
+        private static char[] GeneratePolymer(Dictionary<string, char[]> rules, char[] template, int steps)
+        {
+            char[] polymer = null;
+
+            var replacementSize = rules.First().Value.LongLength;
+            for (int step = 0; step < steps; step++)
             {
                 if (polymer != null)
                 {
                     template = polymer;
                 }
-                polymer = new List<char>();
 
-                for (int i = 1; i < template.Count; i++)
+                long newSize = (template.LongLength * (replacementSize + 1)) - (replacementSize);
+                if (newSize > uint.MaxValue)
+                {
+                    throw new IndexOutOfRangeException($"{newSize} > {uint.MaxValue}!");
+                }
+
+                polymer = new char[newSize];
+                long polymerIndex = 0;
+
+                for (long i = 1; i < template.LongLength; i++)
                 {
                     var element1 = template[i - 1];
                     var element2 = template[i];
                     var key = $"{element1}{element2}";
 
-                    var newElement = rules[key];
+                    var newElements = rules[key];
 
-                    if (i == 1)
+                    if (polymerIndex == 0)
                     {
-                        polymer.Add(element1);
+                        polymer[polymerIndex] = element1;
+                        polymerIndex++;
                     }
 
-                    polymer.Add(newElement);
-                    polymer.Add(element2);
+                    foreach (var newElement in newElements)
+                    {
+                        polymer[polymerIndex] = newElement;
+                        polymerIndex++;
+                    }
+
+                    polymer[polymerIndex] = element2;
+                    polymerIndex++;
                 }
             }
 
             return polymer;
         }
 
-        private static int CalculateResult(Dictionary<string, char> rules, IList<char> polymer)
+        private static int CalculateResult(char[] polymer)
         {
-            var smallestCount = polymer.Count;
+            var smallestCount = polymer.Length;
             var largestCount = 0;
-            foreach (var element in rules.Values.Distinct())
+            foreach (var element in polymer.Distinct())
             {
                 var elementCount = polymer.Count(c => c == element);
 
@@ -92,6 +131,49 @@ namespace Solutions
             }
 
             return largestCount - smallestCount;
+        }
+
+        private static long CalculateResultWithOneRemainingStep(Dictionary<string, char[]> initialRules, Dictionary<string, char[]> expandedRules, char[] polymer)
+        {
+            // Calculate the character counts used for each expansion rule
+            var rulesToCountMapping = new Dictionary<string, Dictionary<char, long>>();
+            foreach (var rule in expandedRules)
+            {
+                var newElements = rule.Value;
+                var distinctCharacters = newElements.Distinct().ToList();
+
+                var mapping = new Dictionary<char, long>();
+                foreach (var character in distinctCharacters)
+                {
+                    mapping.Add(character, newElements.Count(c => c == character));
+                }
+
+                rulesToCountMapping.Add(rule.Key, mapping);
+            }
+
+            // Do the final expansion uing the character counts
+            var results = initialRules.Select(ir => ir.Value.Single()).Distinct().ToDictionary(e => e, _ => 0L);
+            for (long i = 1; i < polymer.LongLength; i++)
+            {
+                var element1 = polymer[i - 1];
+                var element2 = polymer[i];
+                var key = $"{element1}{element2}";
+
+                var mapping = rulesToCountMapping[key];
+                foreach (var kvp in mapping)
+                {
+                    results[kvp.Key] += kvp.Value;
+                }
+
+                if (i == 1)
+                {
+                    results[element1]++;
+                }
+
+                results[element2]++;
+            }
+
+            return results.Values.Max() - results.Values.Min();
         }
     }
 }
